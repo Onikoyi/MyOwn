a23b0b836cc3e8e1fcdd4a196a2604b2bb9d08ef
Your Name <onikstib2007@yahoo.com>
2018-09-01 11:53:51 +0100

    The Other one

---
+++ script.js
@@ -0,0 +1,23 @@
+alert ("Welcome Onikoyi");
+
+let name = prompt ("What is your name?");
+alert ("Welcome  " + name +" !");
+
+alert ("Lets Calculate Average");
+
+let number1 = prompt("Enter first number");
+let number2 = prompt("Enter second number");
+let number3 = prompt("Enter third number");
+let number4 = prompt("Enter fourth number");
+let number5 = prompt("Enter fifth number");
+
+let num1 = parseInt(number1);
+let num2 = parseInt(number2);
+let num3 = parseInt(number3);
+let num4 = parseInt(number4);
+let num5 = parseInt(number5);
+
+
+let sumOfnumbers = num1 + num2 + num3 + num4 +num5;
+// LET AVERAGE
+let Average = sumOfnumbers/5;
